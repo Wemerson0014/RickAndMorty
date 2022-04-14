2d11fa7b273ffe3d9a89a851318166c6c09d18b5
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2022-04-14 13:57:09 -0300

    Parsing dos dados

--- app/src/main/java/com/estudo/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/estudo/rickandmorty/MainActivity.kt
@@ -3,6 +3,8 @@ package com.estudo.rickandmorty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView)
         val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://rickandmortyapi.com/api/")
@@ -25,12 +28,20 @@ class MainActivity : AppCompatActivity() {
         val rickAndMortyServices: RickAndMortyServices =
             retrofit.create(RickAndMortyServices::class.java)
 
-        rickAndMortyServices.getCharacterById().enqueue(object : Callback<Any> {
-            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+        rickAndMortyServices.getCharacterById(54).enqueue(object : Callback<GetCharacterByIdResponse> {
+            override fun onResponse(call: Call<GetCharacterByIdResponse>, response: Response<GetCharacterByIdResponse>) {
                 Log.i("MainActivity", response.toString())
+                if (!response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "Unsuccessful network call", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                val body = response.body()!!
+                val name = body.name
+                textView.text = name
             }
 
-            override fun onFailure(call: Call<Any>, t: Throwable) {
+            override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
                 Log.i("MainActivity", t.message ?: "null message")
             }
         })
