f5422e504c619b23720a288bc28eb384db6d6b41
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2022-04-19 14:56:01 -0300

    Adiciona dependencia do ViewModel e separa classes para adicionar a arquitetura MVVM

--- app/src/main/java/com/estudo/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/estudo/rickandmorty/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.lifecycle.ViewModelProvider
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.picasso.Picasso
@@ -17,6 +18,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: SharedViewModel by lazy {
+        ViewModelProvider(this).get(SharedViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,41 +34,26 @@ class MainActivity : AppCompatActivity() {
         val headerImageView = findViewById<AppCompatImageView>(R.id.headerImageView)
         val genderImageView = findViewById<AppCompatImageView>(R.id.genderImageView)
 
-        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://rickandmortyapi.com/api/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        val rickAndMortyServices: RickAndMortyServices =
-            retrofit.create(RickAndMortyServices::class.java)
-
-        rickAndMortyServices.getCharacterById(54).enqueue(object : Callback<GetCharacterByIdResponse> {
-            override fun onResponse(call: Call<GetCharacterByIdResponse>, response: Response<GetCharacterByIdResponse>) {
-                Log.i("MainActivity", response.toString())
-                if (!response.isSuccessful) {
-                    Toast.makeText(this@MainActivity, "Unsuccessful network call", Toast.LENGTH_SHORT).show()
-                    return
-                }
-
-                val body = response.body()!!
-                nameTextView.text = body.name
-                aliveTextView.text = body.status
-                speciesTextView.text = body.species
-                originTextView.text = body.origin.name
-
-                if (body.gender.equals("male", true)) {
-                    genderImageView.setImageResource(R.drawable.ic_male_24)
-                } else {
-                    genderImageView.setImageResource(R.drawable.ic_female_24)
-                }
-
-                Picasso.get().load(body.image).into(headerImageView)
+        viewModel.refreshCharacter(54)
+        viewModel.characterByIdLiveData.observe(this) { response ->
+            if (response == null) {
+                Toast.makeText(this@MainActivity, "Unsuccessful network call", Toast.LENGTH_SHORT)
+                    .show()
+                return@observe
             }
 
-            override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
-                Log.i("MainActivity", t.message ?: "null message")
+            nameTextView.text = response.name
+            aliveTextView.text = response.status
+            speciesTextView.text = response.species
+            originTextView.text = response.origin.name
+
+            if (response.gender.equals("male", true)) {
+                genderImageView.setImageResource(R.drawable.ic_male_24)
+            } else {
+                genderImageView.setImageResource(R.drawable.ic_female_24)
             }
-        })
+
+            Picasso.get().load(response.image).into(headerImageView)
+        }
     }
 }
---
+++ app/src/main/java/com/estudo/rickandmorty/SharedRepository.kt
@@ -0,0 +1,12 @@
+package com.estudo.rickandmorty
+
+class SharedRepository {
+
+    suspend fun getCharacterById(characterId: Int): GetCharacterByIdResponse? {
+        val request = NetworkLayer.apiClient.getCharacterById(characterId)
+        if (request.isSuccessful) {
+            return request.body()!!
+        }
+        return null
+    }
+}
